5ccc0c1f8b231ba9db7626afe5373b072263ae23
armand.meppa <armand.meppa@tonkain.com>
2022-10-30 15:44:40 +0100

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDOek1vLzg1zdoYanSmZgqQjtPznnbxYYw",
+    authDomain: "oshop-fe004.firebaseapp.com",
+    projectId: "oshop-fe004",
+    storageBucket: "oshop-fe004.appspot.com",
+    messagingSenderId: "1085267234226",
+    appId: "1:1085267234226:web:d6294a2e5d1bbf166d28ab"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDOek1vLzg1zdoYanSmZgqQjtPznnbxYYw",
+    authDomain: "oshop-fe004.firebaseapp.com",
+    projectId: "oshop-fe004",
+    storageBucket: "oshop-fe004.appspot.com",
+    messagingSenderId: "1085267234226",
+    appId: "1:1085267234226:web:d6294a2e5d1bbf166d28ab"
+  }
 };
 
 /*
